46d5c0ff396f8403e73105c9ebe31aed5064273d
RomanovaNI <romanovanataliade@gmail.com>
2021-06-14 13:17:52 +0200

    logs

--- src/test/java/com/example/testTask/fw/ApplicationManager.java
+++ src/test/java/com/example/testTask/fw/ApplicationManager.java
@@ -2,23 +2,36 @@ package com.example.testTask.fw;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager  {
+public class ApplicationManager {
     public WebDriver wd;
     SearchHelper search;
     FilterHelper filter;
+    String browser;
+
+    public ApplicationManager(String browser) {
+
+        this.browser = browser;
+    }
 
 
     public void init() {
-        wd = new ChromeDriver();
+        if(browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)){
+            wd=new FirefoxDriver();
+        }
+
         wd.manage().window().maximize();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.navigate().to("https://yandex.ru/");
 
-        search=new SearchHelper(wd);
-        filter=new FilterHelper(wd);
+        search = new SearchHelper(wd);
+        filter = new FilterHelper(wd);
     }
 
     public SearchHelper search() {
--- src/test/java/com/example/testTask/tests/TestBase.java
+++ src/test/java/com/example/testTask/tests/TestBase.java
@@ -1,22 +1,31 @@
 package com.example.testTask.tests;
 
 import com.example.testTask.fw.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
 
-    protected ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    Logger logger= LoggerFactory.getLogger(TestBase.class);
 
     @BeforeMethod
-    public void SetUp() {
+    public void SetUp(Method m) {
         app.init();
+        logger.info("Start test " +m.getName());
 
     }
 
-    @AfterMethod(enabled = false)
-    public void tearDown() {
+    @AfterMethod(enabled = true)
+    public void tearDown(Method m) {
         app.stop();
+        logger.info("Stop test " + m.getName());
     }
 
 }
